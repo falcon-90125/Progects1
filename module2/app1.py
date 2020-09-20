dcf3a50da74748479bc0bd04f6c3be41d5b00cf2
falcon-90125 <sokolov_aa@ngs.ru>
2020-09-20 18:44:52 +0700

    end

---
+++ module2/app1.py
@@ -0,0 +1,15 @@
+v_str = 'Строка'
+v_int = 240 / 2
+v_float = 240.0
+
+print(type(v_str))
+print(type(v_int))
+print(type(v_float))
+
+v_str = 'Строка'
+v_int = str(240 / 2)
+v_float = str(240.0)
+
+print(type(v_str))
+print(type(v_int))
+print(type(v_float))
