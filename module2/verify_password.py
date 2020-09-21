652e5b3af3fda7eed78ad84aca06fcb79fee91d2
Aleksey Sokolov <sokolov_aa@ngs.ru>
2020-09-21 23:40:46 +0700

    2020-09-21(4)

--- module2/verify_password.py
+++ module2/verify_password.py
@@ -1,11 +1,13 @@
 password = input("Введите пароль: ")
+
 try:
     result = 1/len(password) # условие для исключения пустого пароля
     result = int(password) # условие для исключения пароля "только цифры"
     print("Ваш пароль состоит только из цифр")
+
 except ZeroDivisionError:
     print("Вы ввели пустой пароль")
-    result = 0
+    
 except ValueError: # если int в условии не смог преобразовать пароль значит есть буквы
     print("Требования к паролю соблюдены")
-    result = 0
+
