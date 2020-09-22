13e876de84d9b002529fabd3f38fccfdcb6ca900
falcon-90125 <sokolov_aa@ngs.ru>
2020-09-22 23:47:06 +0700

    2020-09-22

---
+++ module2/processing_dictionaries_and_lists.py
@@ -0,0 +1,19 @@
+account1 = {'login': 'ivan', 'password': 'q1'}
+account2 = {'login': 'petr', 'password': 'q2'}
+account3 = {'login': 'olga', 'password': 'q3'}
+account4 = {'login': 'anna', 'password': 'q4'}
+
+user1 = {'name': 'Иван', 'age': 20, 'account': account1}
+user2 = {'name': 'Петр', 'age': 25, 'account': account2}
+user3 = {'namt': 'Ольга', 'age': 18, 'account': account3}
+user4 = {'name': 'Анна', 'age': 27, 'account': account4}
+
+user_list = [user1, user2, user3, user4]
+
+key = input("Введите ключ (name или account): ")
+
+try:
+    key.lower() == 'name'
+    print("OK")
+except:
+    print("bad")
