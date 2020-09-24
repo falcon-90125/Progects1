809e59ac31df856225430906a42bc36291ffc190
falcon-90125 <sokolov_aa@ngs.ru>
2020-09-24 21:03:32 +0700

    111

--- module2/processing_dictionaries_and_lists.py
+++ module2/processing_dictionaries_and_lists.py
@@ -3,22 +3,27 @@ account2 = {'login': 'petr', 'password': 'q2'}
 account3 = {'login': 'olga', 'password': 'q3'}
 account4 = {'login': 'anna', 'password': 'q4'}
 
-user1 = {'name': 'Иван', 'age': 20, 'account': account1}
-user2 = {'name': 'Петр', 'age': 25, 'account': account2}
-user3 = {'name': 'Ольга', 'age': 18, 'account': account3}
-user4 = {'name': 'Анна', 'age': 27, 'account': account4}
+user1 = {'name': 'Иван', 'age': 20, 'account': 'account1'}
+user2 = {'name': 'Петр', 'age': 25, 'account': 'account2'}
+user3 = {'name': 'Ольга', 'age': 18, 'account': 'account3'}
+user4 = {'name': 'Анна', 'age': 27, 'account': 'account4'}
 
 user_list = [user1, user2, user3, user4]
 
-key = input("Введите ключ (name или account): ")
-user_key = key.lower()
+# key = input("Введите ключ (name или account): ")
+# user_key = key.lower()
 
-try:
-    len(user_key)<4
-    print(f"значение ключа {user_key} для юзера 1 = {user1[user_key]}")
-    print(f"значение ключа {user_key} для юзера 2 = {user2[user_key]}")
-    print(f"значение ключа {user_key} для юзера 3 = {user3[user_key]}")
-    print(f"значение ключа {user_key} для юзера 4 = {user4[user_key]}")
+# try:
+#     len(user_key)<4
+#     print(f'значение ключа {user_key} для юзера 1 = {user1[user_key]}')
+#     print(f'значение ключа {user_key} для юзера 2 = {user2[user_key]}')
+#     print(f'значение ключа {user_key} для юзера 3 = {user3[user_key]}')
+#     print(f'значение ключа {user_key} для юзера 4 = {user4[user_key]}')
 
-except KeyError:
-    print ("Введенный ключ не найден")
+# except KeyError:
+#     print ("Введенный ключ не найден")
+
+serial_number = input('Введите порядковый номер: ')
+
+print ('Данные по юзеру №' + str(serial_number) + ':')
+print (user_list[serial_number])
