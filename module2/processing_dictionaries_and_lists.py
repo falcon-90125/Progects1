efe10512b95c6f74b1b41760746b8780c2471391
falcon-90125 <sokolov_aa@ngs.ru>
2020-09-26 23:11:59 +0700

    2020-09-29

--- module2/processing_dictionaries_and_lists.py
+++ module2/processing_dictionaries_and_lists.py
@@ -22,16 +22,42 @@ user_list = [user1, user2, user3, user4]
 # except KeyError:
 #     print ("Введенный ключ не найден")
 
-sequence_number = int(input('Введите порядковый номер: ')) #порядковый номер
-#sequence_number_adapted = int(sequence_number) - 1 #порядковый номер адаптированный под user_list - может не пригодится
+# sequence_number = int(input('Введите порядковый номер: ')) #порядковый номер
+# #sequence_number_adapted = int(sequence_number) - 1 #порядковый номер адаптированный под user_list - может не пригодится
 
-if sequence_number > 4:
-    print('Пользователь с указанным номером не найден')
-elif sequence_number < 1:
-    print('Пользователь с указанным номером не найден')
-elif sequence_number == 2:
-    print ('Данные по юзеру №' + str(sequence_number) + ':')
-elif sequence_number == 3:
-    print ('Данные по юзеру №' + str(sequence_number) + ':')
-else:
-    print ('Данные по юзеру №' + str(sequence_number) + ':')
+# if sequence_number > 4:
+#     print('Пользователь с указанным номером не найден')
+# elif sequence_number < 1:
+#     print('Пользователь с указанным номером не найден')
+# elif sequence_number == 1:
+#     print ('имя: Иван')
+#     print ('возраст: 20')
+#     print ('логин: ivan')
+#     print ('пароль: q1')
+# elif sequence_number == 2:
+#     print ('Данные по юзеру №' + str(sequence_number) + ':')
+#     print ('имя: Петр')
+#     print ('возраст: 25')
+#     print ('логин: petr')
+#     print ('пароль: q2')
+# elif sequence_number == 3:
+#     print ('Данные по юзеру №' + str(sequence_number) + ':')
+#     print ('имя: Ольга')
+#     print ('возраст: 18')
+#     print ('логин: olga')
+#     print ('пароль: q3')
+# else:
+#     print ('Данные по юзеру №' + str(sequence_number) + ':')
+#     print ('имя: Анна')
+#     print ('возраст: 27')
+#     print ('логин: anna')
+#     print ('пароль: q4')
+
+user_number = int(input('Введите номер пользователя, которого нужно переместить в конец: '))
+user_number_adapted = int(user_number) - 1 # порядковый номер адаптированный под user_list
+print('Список до изменения:')
+print(user_list)
+user_list.append(user_list[user_number_adapted])
+user_list.remove(user_list[user_number_adapted])
+print('Список после изменения:')
+print(user_list)
