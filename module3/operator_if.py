021123c4502b368a12506c441d291450d970803d
falcon-90125 <sokolov_aa@ngs.ru>
2020-09-30 22:29:19 +0700

    2020-09-30

--- module3/operator_if.py
+++ module3/operator_if.py
@@ -1,13 +1,24 @@
+# Турист Мария возраст 22. Посетил город Париж
+
 cities = ['Москва', 'Париж', 'Лондон']
 
 users = [{'name': 'Иван', 'age': 35}, {'name': 'Мария', 'age': 22}, {'name': 'Соня', 'age': 20}]
 
 tourists = [{'user': users[0], 'city': cities[0]}, {'user': users[1], 'city': cities[1]}, {'user': users[2], 'city': cities[2]}]
 
-#city = input('Введите город: ')
+city = input('Введите город: ')
 
-# print (f"Турист {users['name']} возраст {users['age']}.")
+user0 = users[0]
+user1 = users[1]
+user2 = users[2]
 
-#element1 = cities.index(city)
-print(tourists)
-# [{'user': {'name': 'Иван', 'age': 35}, 'city': 'Москва'}, {'user': {'name': 'Мария', 'age': 22}, 'city': 'Париж'}, {'user': {'name': 'Соня', 'age': 20}, 'city': 'Лондон'}]
+if city == (cities[0]):
+    print (f"Турист {user0['name']} возраст {user0['age']}.")
+else:
+    if city == (cities[1]):
+        print (f"Турист {user1['name']} возраст {user1['age']}.")
+    else:
+        if city == (cities[2]):
+            print (f"Турист {user2['name']} возраст {user2['age']}.")
+        else:
+            print ('Нет такого города')
