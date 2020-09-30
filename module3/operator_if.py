a8aab9b1c4998afc27a481bdc16bf503461ad849
Aleksey Sokolov <sokolov_aa@ngs.ru>
2020-09-30 23:58:27 +0700

    2020-09-30(2)

--- module3/operator_if.py
+++ module3/operator_if.py
@@ -4,10 +4,16 @@ users = [{'name': 'Иван', 'age': 35}, {'name': 'Мария', 'age': 22}, {'n
 
 tourists = [{'user': users[0], 'city': cities[0]}, {'user': users[1], 'city': cities[1]}, {'user': users[2], 'city': cities[2]}]
 
-#city = input('Введите город: ')
+city = input('Введите город: ')
+city_adapted = city.lower().title() # адаптация регистров
 
-# print (f"Турист {users['name']} возраст {users['age']}.")
-
-#element1 = cities.index(city)
-print(tourists)
-# [{'user': {'name': 'Иван', 'age': 35}, 'city': 'Москва'}, {'user': {'name': 'Мария', 'age': 22}, 'city': 'Париж'}, {'user': {'name': 'Соня', 'age': 20}, 'city': 'Лондон'}]
+if city_adapted == (cities[0]): # сравнение введёного города со списком cities
+    print (f"Турист {users[0]['name']} возраст {users[0]['age']}. Посетил город {city_adapted}") # вывод имени, возраста туриста и города посещения, соотв. введённому
+else:
+    if city_adapted == (cities[1]): # сравнение введёного города со списком cities
+        print (f"Турист {users[1]['name']} возраст {users[1]['age']}. Посетил город {city_adapted}") # вывод имени, возраста туриста и города посещения, соотв. введённому
+    else:
+        if city_adapted == (cities[2]): # сравнение введёного города со списком cities
+            print (f"Турист {users[2]['name']} возраст {users[2]['age']}. Посетил город {city_adapted}") # вывод имени, возраста туриста и города посещения, соотв. введённому
+        else:
+            print ('Такой город ни кто не посещал') # если введён город не из списка
