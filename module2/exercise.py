8ace6321fbf1a00191f2d29e633b6e531d603b32
Aleksey Sokolov <sokolov_aa@ngs.ru>
2020-10-06 08:18:05 +0700

    2020-10-06

--- module2/exercise.py
+++ module2/exercise.py
@@ -1,10 +1,25 @@
-student = {'name': 'Иван', 'age': 18, 'cities': ['Москва', 'Лондон']}
+student1 = {'name': 'Иван', 'cities': ['Москва', 'Лондон']}
+student2 = {'name': 'Мария', 'cities': ['Париж', 'Нью Йорк']}
+student3 = {'name': 'Петр', 'cities': ['Пекин', 'Токио']}
 
-# Выводим все ключи словаря через метод keys()
-print(student.keys())
+students = [student1, student2, student3]
 
-# Выводим все значения словаря через метод values()
-print(student.values())
+city = 'Париж'
 
-# Выводим все элементы словаря через метод items()
-print(student.items())
+name = ''
+iterations = 0
+for student in students:
+    iterations += 1
+    if city in student['cities']:
+        name = student['name']
+print(f'Количество итераций: {iterations}')
+
+name = ''
+iterations = 0
+for student in students:
+    iterations += 1
+    if city in student['cities']:
+        name = student['name']
+    if name:
+        break # прерываем цикл если нашли студента
+print(f'Количество итераций: {iterations}')
