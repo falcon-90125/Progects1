50864bdf29d1f0690970a286735b234e35b1bddc
Aleksey Sokolov <sokolov_aa@ngs.ru>
2020-10-07 19:49:50 +0700

    2020-10-07

--- module2/exercise.py
+++ module2/exercise.py
@@ -1,25 +1,2 @@
-student1 = {'name': 'Иван', 'cities': ['Москва', 'Лондон']}
-student2 = {'name': 'Мария', 'cities': ['Париж', 'Нью Йорк']}
-student3 = {'name': 'Петр', 'cities': ['Пекин', 'Токио']}
-
-students = [student1, student2, student3]
-
-city = 'Париж'
-
-name = ''
-iterations = 0
-for student in students:
-    iterations += 1
-    if city in student['cities']:
-        name = student['name']
-print(f'Количество итераций: {iterations}')
-
-name = ''
-iterations = 0
-for student in students:
-    iterations += 1
-    if city in student['cities']:
-        name = student['name']
-    if name:
-        break # прерываем цикл если нашли студента
-print(f'Количество итераций: {iterations}')
+while input ("закончить? (y/n): ") != 'y':
+    print('ещё одна итерация')
